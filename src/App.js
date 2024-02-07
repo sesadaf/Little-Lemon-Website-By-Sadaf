2396ba395158fe1aff7467ebc32cf1ef894c7972
sesadaf <157764587+sesadaf@users.noreply.github.com>
2024-02-07 12:18:32 -0600

    Installed a fresh repository react app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 
 import './App.css';
 import Header from './components/Header';
-import About from './components/About';
-import  Footer from './components/BottomNavigation';
-
-import  Highlights from './components/Highlights';
-
-
-import Hero from './components/Hero';
-import Testimonials from './components/Testimonials';
 
 
 function App() {
   return (
     <section>
-        <Header/>
-        <Hero/>
-        <Highlights/>
-       
-        <Testimonials/>
-        <About/>
+      <Header/>
 
-
-        <Footer/>
-
-    </section>
-  
+    </section>>
   );
 }
 
