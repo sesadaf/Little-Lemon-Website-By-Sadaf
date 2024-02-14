75658ff92191df1f61d14768de45df0f12bd0062
sesadaf <157764587+sesadaf@users.noreply.github.com>
2024-02-14 09:19:42 -0600

    Added About section and made homeage responsive

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,30 @@
-function About(){
+import image1 from '../components/assets/Mario and Adrian A.jpg'
+import image2 from '../components/assets/Mario and Adrian b.jpg'
+
+import "../components/styles/About.css"
 
 
+const  About = () =>  {
+
+return(
+    <article className="about-us">
+    <section className="hero-text">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p className="about-subtext">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. 
+        Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, 
+        and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers 
+        continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
+    </section>
+
+    <section className="double-image">
+    
+        <img className="about-2" src={image2} alt="Little Lemon restaurant cuisine 2"></img>
+        <img className="about-1" src={image1} alt="Little Lemon restaurant cuisine 1"></img>
+        
+    </section>
+</article>
+)
 
 
 
